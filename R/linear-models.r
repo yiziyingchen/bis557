dc2519de1ef74ab6fae1c78d80123bf9c6e65add
Yiziying Chen <yiziyingchen@vpn172022122244.its.yale.internal>
2018-09-21 08:37:30 -0400

    check the result

--- R/linear-models.r
+++ R/linear-models.r
@@ -7,9 +7,18 @@
 #' @return An lm object
 #' @importFrom stats lm
 #' @examples
-#' fit <- linear_model(Sepal.Length ~., iris)
-#' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+    X = model.matrix(formula, data)
+    Y= model.frame(formula,data)[,1]
+    beta <- qr.solve(X,Y)
+    for (i in 1:length(beta)){
+        if (beta[i] == 0)
+        beta[i] = NA
+        
+    }
+    ret <- list()
+    ret$coefficients = c(beta)
+    class(ret) = "lm"
+    return(ret)
 }
